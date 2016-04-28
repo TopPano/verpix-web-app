4e4d8146e3a885a711094153d28408cdfc14e848
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-04-28 07:34:15 +0000

    Modify Gallery.jsx

--- shared/components/Gallery.jsx
+++ shared/components/Gallery.jsx
@@ -35,24 +35,27 @@ export default class Gallery extends Component{
     return Math.floor(el.clientWidth) - parseInt(style.getPropertyValue('padding-left')) - parseInt(style.getPropertyValue('padding-right'));
   }
   render(){
-    let numPerRow = Math.ceil(this.state.containerWidth / this.props.maxWidth);
+    const { posts, maxWidth, ratio } = this.props;
+    let numPerRow = Math.ceil(this.state.containerWidth / maxWidth);
     let paddingLeft = 5, paddingRight = 5;
     let postWidth =
-      this.props.posts.length >= numPerRow ?
-      Math.floor(this.state.containerWidth / numPerRow) - paddingLeft - paddingRight :
-      this.props.maxWidth - paddingLeft - paddingRight;
-    let postHeight = Math.floor(postWidth / this.props.ratio);
+        posts.page.count >= numPerRow ?
+        Math.floor(this.state.containerWidth / numPerRow) - paddingLeft - paddingRight :
+        maxWidth - paddingLeft - paddingRight;
+    let postHeight = Math.floor(postWidth / ratio);
     let wrapperWidth = (postWidth + paddingLeft + paddingRight) * numPerRow;
     let previews = [];
 
-    this.props.posts.map((post, k) => {
+    posts.feed.map((post, k) => {
+      const { sid, thumbnailUrl, likes } = post;
+      let linkUrl = 'http://dev.verpix.net/?post=' + sid;
       previews.push(
         <View
           key={ k }
-          linkUrl={ post.linkUrl }
-          imgUrl={ post.imgUrl }
-          initialCount={ post.count }
-          initialIsLiked={ post.isLiked }
+          linkUrl={ linkUrl }
+          imgUrl={ thumbnailUrl }
+          initialCount={ likes.count }
+          initialIsLiked={ likes.isLiked }
           width={ postWidth }
           height={ postHeight }
         />
@@ -71,15 +74,7 @@ export default class Gallery extends Component{
 Gallery.displayName = 'Gallery';
 
 Gallery.propTypes = {
-  posts: PropTypes.arrayOf(
-    PropTypes.shape({
-      postId: PropTypes.string.isRequired,
-      linkUrl: PropTypes.string.isRequired,
-      thumbnailUrl: PropTypes.string.isRequired,
-      count: PropTypes.number.isRequired,
-      isLiked: PropTypes.bool.isRequired
-    })
-  ).isRequired,
+  posts: PropTypes.object.isRequired,
   maxWidth: PropTypes.number.isRequired,
   ratio: PropTypes.number.isRequired
 };
