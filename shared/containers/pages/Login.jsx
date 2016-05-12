ed7e30cf95004ffa094edd51952900bb7e070af8
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-05-12 03:22:41 +0000

    Add modified Login container.

--- shared/containers/pages/Login.jsx
+++ shared/containers/pages/Login.jsx
@@ -3,37 +3,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Login from '../../components/Login';
 import { loginUser } from '../../actions/user';
 
 class LoginPageContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '' };
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const email = this.refs.email.value.trim();
-    const password = this.refs.pass.value.trim();
+  login = (email, password) => {
     this.props.dispatch(loginUser({email, password}));
-  }
-
-  handleChange(event) {
-    this.setState({email: event.target.value});
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)} style={{ color: 'black' }}>
-        <label>
-          <input type="email" ref="email" placeholder="email" value={this.state.email} defaultValue="joe@example.com"
-            onChange={this.handleChange.bind(this)} />
-        </label>
-        <label>
-          <input type="password" ref="pass" placeholder="password" />
-        </label> (hint: password1)<br />
-        <button type="submit">login</button>
-      </form>
+      <Login loginUser={this.login} />
     );
   }
 }
