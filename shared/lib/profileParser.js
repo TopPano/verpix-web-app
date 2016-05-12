4990d813373fb7e54cdf101cf225b1d91bfb5bcb
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-05-12 12:57:32 +0000

    Modify functon of parsing profile photo URL.

--- shared/lib/profileParser.js
+++ shared/lib/profileParser.js
@@ -7,9 +7,12 @@ export function parseUsername(user) {
 }
 
 export function parseProfilePhotoUrl(user) {
+  /*
   let profilePhotoUrl =
       user.identities.length > 0 ?
       user.identities[0].profile.photos[0].value :
       user.profilePhotoUrl;
   return profilePhotoUrl ? profilePhotoUrl : DEFAULT_PROFILE_PHOTO_URL;
+  */
+  return user.profilePhotoUrl ? user.profilePhotoUrl : DEFAULT_PROFILE_PHOTO_URL;
 }
