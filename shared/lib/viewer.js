212f52a8fbc3bfb1c5d94c1aa4ec9aeb8bcc4536
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-05-18 09:58:19 +0000

    Modify lib/viewer.js

--- shared/lib/viewer.js
+++ shared/lib/viewer.js
@@ -161,7 +161,6 @@ TOPPANO.modelInit = function(modelId) {
                      var first_node_ID = Object.keys(TOPPANO.gv.nodes_meta)[0];
                      TOPPANO.gv.current_node_ID = first_node_ID;
                      TOPPANO.buildScene(first_node_ID);
-                     TOPPANO.readInitCamParams();
                  });
     }).done(function() {
         // add listener
@@ -297,23 +296,6 @@ TOPPANO.buildScene = function(node_ID){
     }
 }
 
-// Read the initial camera parameters from url query string.
-TOPPANO.readInitCamParams = function() {
-    let fov = parseInt(getUrlParam('fov')),
-        lat = parseInt(getUrlParam('lat')),
-        lng = parseInt(getUrlParam('lng'));
-
-    if(!isNaN(fov)) {
-        TOPPANO.gv.cam.camera.fov = Math.max(TOPPANO.gv.para.fov.min, Math.min(TOPPANO.gv.para.fov.max, fov));
-    }
-    if(!isNaN(lat)) {
-        TOPPANO.gv.cam.lat = Math.max(-85, Math.min(85, lat));
-    }
-    if(!isNaN(lng)) {
-        TOPPANO.gv.cam.lng = (lng + 360) % 360;
-    }
-};
-
 // add listeners
 TOPPANO.addListener = function() {
     TOPPANO.setCursorHandler();
@@ -322,13 +304,22 @@ TOPPANO.addListener = function() {
 };
 
 // setting global variables for initialization
-TOPPANO.initGV = function(para){
-    if (para.zoom) {
-        TOPPANO.gv.cam.defaultCamFOV = clamp(para.zoom, TOPPANO.gv.para.fov.min, TOPPANO.gv.para.fov.max);
+TOPPANO.initGV = function(params){
+  const { cam } = params;
+  if(cam) {
+    if(cam.lat) {
+      TOPPANO.gv.cam.lat = clamp(cam.lat, -85, 85);
     }
-    if (para.canvas) {
-        TOPPANO.gv.canvasID = para.canvas;
+    if(cam.lng) {
+      TOPPANO.gv.cam.lng = (cam.lng + 360) % 360;
     }
+    if(cam.fov) {
+      TOPPANO.gv.cam.defaultCamFOV = clamp(cam.fov, TOPPANO.gv.para.fov.min, TOPPANO.gv.para.fov.max);
+    }
+  }
+  if(params.canvas) {
+    TOPPANO.gv.canvasID = params.canvas;
+  }
 };
 
 // renderer setting
@@ -442,37 +433,6 @@ TOPPANO.updateCurrentUrl = function() {
 function clamp(number, min, max) {
     return Math.min(Math.max(number, min), max);
 }
-
-// Get a URL parameter's value by name.
-function getUrlParam(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-
-    var queryStr = location.search.substr(1),
-        regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-        results;
-    if(queryStr.indexOf('post=') > -1) {
-        // Query string contains 'post=', it means query string is not encoded with base64.
-        results = regex.exec('?' + queryStr);
-    } else {
-        // Query string is encoded with base64, decode it.
-        results = regex.exec('?' + queryStr);
-        //results = regex.exec('?' + base64Convert(queryStr, 'decode'));
-    }
-    return results === null ? '' : decodeURIComponent(results[1]);
-    //return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-// Encode/Decode string with base64.
-/*
-function base64Convert(str64, action) {
-    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-    if(action === 'encode') {
-        return Base64.encode(str64);
-    } else {
-        return Base64.decode(str64);
-    }
-}
-*/
 
 function set_on_rotating_scene(){
     // rotating-scene
