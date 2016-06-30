0f148cc33247b5a0db1d1cbce8cf5041b4ea4ed8
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-06-30 03:40:46 +0000

    Modify contents about sharing.

--- server/utils/renderSharePropsHtml.js
+++ server/utils/renderSharePropsHtml.js
@@ -5,13 +5,18 @@ import externalApiConfig from 'etc/external-api';
 import clientConfig from 'etc/client';
 import api from 'lib/api';
 
+import SITE_CONTENT from 'content/site/en-us.json';
+import VIEWER_CONTENT from 'content/viewer/en-us.json';
+import { MEDIA_TYPE, ORIENTATION, SHARE_IMAGE_SIZE } from 'constants/common';
+import { SITE_SHARE_IMAGE } from 'constants/site';
+
 export default function renderSharePropsHtml(req, isViewerPage) {
   const defaultContent = {
-    image: `${clientConfig.staticUrl}/static/images/fb-share-default.jpg`,
-    imageWidth: 600,
-    imageHeight: 315,
-    title: 'LOOK it\'s my awesome 360 photo!!',
-    description: 'Register ＆ add your friends in Verpix to join more activities.',
+    image: `${clientConfig.staticUrl}${SITE_SHARE_IMAGE}`,
+    imageWidth: SHARE_IMAGE_SIZE.LANDSCAPE.WIDTH,
+    imageHeight: SHARE_IMAGE_SIZE.LANDSCAPE.HEIGHT,
+    title: SITE_CONTENT.SHARE.DEFAULT_CAPTION,
+    description: SITE_CONTENT.SHARE.DEFAULT_DESCRIPTION,
     url: `${req.protocol}://${req.get('Host')}${req.url}`
   }
 
@@ -20,7 +25,7 @@ export default function renderSharePropsHtml(req, isViewerPage) {
     // Open Graph tags
     'fb:app_id': `${externalApiConfig.facebook.id}`,
     'og:type': 'website',
-    'og:site_name': 'Verpix',
+    'og:site_name': SITE_CONTENT.SITE_NAME,
     'og:image': defaultContent.image,
     'og:image:width': defaultContent.imageWidth,
     'og:image:height': defaultContent.imageHeight,
@@ -32,13 +37,27 @@ export default function renderSharePropsHtml(req, isViewerPage) {
   if(isViewerPage) {
     const postId = getViewerPostId(req.url);
     requestPostInfo = api.posts.getPost(postId).then((response) => {
+      const defaultViewerContent =
+        response.result.mediaType === MEDIA_TYPE.LIVE_PHOTO ?
+        VIEWER_CONTENT.SHARE.LIVE_PHOTO :
+        VIEWER_CONTENT.SHARE.PANORAMA;
+      const imageSize =
+        response.result.mediaType === MEDIA_TYPE.LIVE_PHOTO && response.result.dimension.orientation === ORIENTATION.PORTRAIT ?
+        SHARE_IMAGE_SIZE.PORTRAIT :
+        SHARE_IMAGE_SIZE.LANDSCAPE;
       const newImage = response.result.thumbnail.srcUrl,
             newTitle = response.result.caption;
       const image = newImage ? newImage : defaultContent.image,
-            title = newTitle ? newTitle : defaultContent.title;
+            imageWidth = imageSize.WIDTH,
+            imageHeight = imageSize.HEIGHT,
+            title = newTitle ? newTitle : defaultViewerContent.DEFAULT_CAPTION,
+            description = defaultViewerContent.DEFAULT_DESCRIPTION;
       shareProps = merge({}, shareProps, {
         'og:image': image,
-        'og:title': title
+        'og:image:width': imageWidth,
+        'og:image:height': imageHeight,
+        'og:title': title,
+        'og:description': description
       });
 
       return renderPropsHtml(shareProps);
