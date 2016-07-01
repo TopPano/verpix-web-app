550dd80eb34638cf512839439f04e5837dee8ec8
SuJiaKuan <jiakuan.su@toppano.in>
2016-07-01 12:42:11 +0800

    Modify Footer component

--- server/development.js
+++ server/development.js
@@ -125,6 +125,9 @@ function renderHTML(html, initialState, config, sharePropsHtml) {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics_debug.js','ga');
       </script>
+      <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+      </script>
     </head>
     <body>
       <div id="app">${html}</div>
