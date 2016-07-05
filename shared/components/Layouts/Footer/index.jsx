37ee65f3b7af9fccdce924ae1a3608fd9be4d8db
SuJiaKuan <jiakuan.su@toppano.in>
2016-07-05 10:07:19 +0800

    Add 'Follow @Verpix' button for Twitter at footer.

--- shared/components/Layouts/Footer/index.jsx
+++ shared/components/Layouts/Footer/index.jsx
@@ -3,37 +3,30 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import SITE_CONTENT from 'content/site/en-us.json';
 import { EXTERNAL_LINKS } from 'constants/common';
 
 if (process.env.BROWSER) {
   require('./Footer.css');
 }
 
-class FooterComponent extends React.Component {
+class Footer extends React.Component {
   render() {
     return (
-      <div className='footer-component navbar-fixed-bottom'>
-        <Link to='/faq' className='footer-item' href={EXTERNAL_LINKS.FAQ} target='_blank'>{'FAQ'}</Link>
-        <a className='footer-item' href={EXTERNAL_LINKS.TERMS_OF_USE} target='_blank'>{'Terms of Use'}</a>
-        <a className='footer-item' href={EXTERNAL_LINKS.PRIVACY_POLICY} target='_blank'>{'Privacy Policy'}</a>
-        <a className='footer-item' href={EXTERNAL_LINKS.FACEBOOK} target='_blank'>
-          <img src='/static/images/footer/link-facebook.png' />
+      <div className="footer-component navbar-fixed-bottom">
+        <Link to="/faq" className="footer-item" href={EXTERNAL_LINKS.FAQ} target="_blank">{SITE_CONTENT.FOOTER.FAQ}</Link>
+        <a className="footer-item" href={EXTERNAL_LINKS.TERMS_OF_USE} target="_blank">{SITE_CONTENT.FOOTER.TERMS_OF_USE}</a>
+        <a className="footer-item" href={EXTERNAL_LINKS.PRIVACY_POLICY} target="_blank">{SITE_CONTENT.FOOTER.PRIVACY_POLICY}</a>
+        <a className="footer-item" href={EXTERNAL_LINKS.FACEBOOK} target="_blank">
+          <img src="/static/images/footer/link-facebook.png" />
         </a>
-        <a className='footer-item' href={EXTERNAL_LINKS.TWITTER} target='_blank'>
-          <img src='/static/images/footer/link-twitter.png' />
+        <a className="footer-item" href={EXTERNAL_LINKS.INSTAGRAM} target="_blank">
+          <img src="/static/images/footer/link-instagram.png" />
         </a>
-        <a className='footer-item' href={EXTERNAL_LINKS.INSTAGRAM} target='_blank'>
-          <img src='/static/images/footer/link-instagram.png' />
-        </a>
+        <a className="footer-item twitter-follow-button" href={EXTERNAL_LINKS.TWITTER} data-show-count="false">{SITE_CONTENT.FOOTER.TWITTER}</a>
       </div>
     );
   }
 }
 
-FooterComponent.displayName = 'LayoutFooterFooterComponent';
-
-// Uncomment properties you need
-// FooterComponent.propTypes = {};
-// FooterComponent.defaultProps = {};
-
-export default FooterComponent;
+export default Footer;
